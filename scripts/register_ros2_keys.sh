000ade03092720ca594de72255d071d885420d06
Masaya Kataoka <ms.kataoka@gmail.com>
2022-11-15 01:24:11 +0900

    use always tag and update script
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- scripts/register_ros2_keys.sh
+++ scripts/register_ros2_keys.sh
@@ -1,17 +1,17 @@
 apt update
-apt install locales
+apt install -y locales
 locale-gen en_US en_US.UTF-8
 update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 
-apt install software-properties-common
+apt install -y software-properties-common
 add-apt-repository universe
 apt update
-apt install curl gnupg2 lsb-release
+apt install -y curl gnupg2 lsb-release
 curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release &&\
     echo $UBUNTU_CODENAME) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 apt update
-apt install python3-argcomplete
+apt install -y python3-argcomplete
--- scripts/register_ros_keys.sh
+++ scripts/register_ros_keys.sh
@@ -1,4 +1,4 @@
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-apt install curl # if you haven't already installed curl
+apt install -y curl # if you haven't already installed curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
 apt update
